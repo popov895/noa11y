d1121d89033b8e8ab96e32357dd4371f8e5bf48e
Eugene Popov <popov895@ukr.net>
2022-05-20 14:35:10 +0300

    Create extension

---
+++ extension.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Main = imports.ui.main;
+
+class Extension {
+    enable() {
+        this._hideAccessibilityMenu();
+        this._sessionModeChangedId = Main.sessionMode.connect(
+            'updated',
+            this._onSessionModeChanged.bind(this)
+        );
+    }
+
+    disable() {
+        Main.sessionMode.disconnect(this._sessionModeChangedId);
+        this._showAccessibilityMenu();
+    }
+
+    _hideAccessibilityMenu() {
+        const a11y = Main.panel.statusArea['a11y'];
+        if (a11y) {
+            a11y.container.hide();
+        }
+    }
+
+    _showAccessibilityMenu() {
+        const a11y = Main.panel.statusArea['a11y'];
+        if (a11y) {
+            a11y.container.show();
+        }
+    }
+
+    _onSessionModeChanged(session) {
+        if (!session.isGreeter) {
+            this._hideAccessibilityMenu();
+        } else {
+            this._showAccessibilityMenu();
+        }
+    }
+}
+
+function init() {
+    return new Extension();
+}

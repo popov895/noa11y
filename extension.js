fea60cc16e8719575a6f824636ff603e26d39b98
Eugene Popov <popov895@ukr.net>
2023-06-15 16:50:04 +0300

    Code improvements

--- extension.js
+++ extension.js
@@ -19,25 +19,15 @@ class Extension {
     }
 
     _hideAccessibilityMenu() {
-        const a11y = Main.panel.statusArea['a11y'];
-        if (a11y) {
-            a11y.container.hide();
-        }
+        Main.panel.statusArea['a11y']?.container.hide();
     }
 
     _showAccessibilityMenu() {
-        const a11y = Main.panel.statusArea['a11y'];
-        if (a11y) {
-            a11y.container.show();
-        }
+        Main.panel.statusArea['a11y']?.container.show();
     }
 
-    _onSessionModeChanged(session) {
-        if (!session.isGreeter) {
-            this._hideAccessibilityMenu();
-        } else {
-            this._showAccessibilityMenu();
-        }
+    _onSessionModeChanged() {
+        this._hideAccessibilityMenu();
     }
 }
 

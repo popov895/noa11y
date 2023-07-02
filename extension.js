8ba57f4dd701b57125783fe7bbf5d7b89fdcf8a2
Eugene Popov <popov895@ukr.net>
2023-07-02 18:24:12 +0300

    Prefer to use template strings instead of regular ones

--- extension.js
+++ extension.js
@@ -6,24 +6,24 @@ class Extension {
     enable() {
         this._hideAccessibilityMenu();
         this._sessionModeChangedId = Main.sessionMode.connect(
-            'updated',
+            `updated`,
             this._onSessionModeChanged.bind(this)
         );
     }
 
     disable() {
-        // This extension uses the 'unlock-dialog' session mode to hide 
+        // This extension uses the 'unlock-dialog' session mode to hide
         // the accessibility button on the lock screen as well.
         Main.sessionMode.disconnect(this._sessionModeChangedId);
         this._showAccessibilityMenu();
     }
 
     _hideAccessibilityMenu() {
-        Main.panel.statusArea['a11y']?.container.hide();
+        Main.panel.statusArea[`a11y`]?.container.hide();
     }
 
     _showAccessibilityMenu() {
-        Main.panel.statusArea['a11y']?.container.show();
+        Main.panel.statusArea[`a11y`]?.container.show();
     }
 
     _onSessionModeChanged() {
